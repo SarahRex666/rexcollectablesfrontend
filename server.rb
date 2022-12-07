b6be5db5e68415a273177883b2e7638e3da7e6f2
SarahRex666 <95315055+SarahRex666@users.noreply.github.com>
2022-12-06 22:03:52 -0500

    chchchchanges

--- server.rb
+++ server.rb
@@ -1,21 +1,34 @@
 require 'sinatra'
 require 'stripe'
 # This is your test secret API key.
-Stripe.api_key = 'pk_test_51M2UQIC0GoTrqNnxTDjV54o5enqNBnx2uew1ozKnYSkfqJelcH5VUx9HunI8DJJNJToWpOnPXROhOQDAT3ohvk6r00QnSf1U5b'
+Stripe.api_key = 'sk_test_51M2UQIC0GoTrqNnxEqDIJEizbilRwBmoxc2lHvCS6s2K8NH5yAo3KHJod3ptBGVRgWLLHBOXD0U9Iw1ecN3kgCt500ovEuJelz'
+
 set :static, true
 set :port, 4242
+
+# Securely calculate the order amount
+def calculate_order_amount(_items)
+  # Replace this constant with a calculation of the order's amount
+  # Calculate the order total on the server to prevent
+  # people from directly manipulating the amount on the client
+  1400
+end
+
+# An endpoint to start the payment process
 post '/create-payment-intent' do
   content_type 'application/json'
   data = JSON.parse(request.body.read)
-# Create a PaymentIntent with amount and currency
+
+  # Create a PaymentIntent with amount and currency
   payment_intent = Stripe::PaymentIntent.create(
     amount: calculate_order_amount(data['items']),
-    currency: 'eur',
+    currency: 'usd',
     automatic_payment_methods: {
       enabled: true,
     },
   )
-{
+
+  {
     clientSecret: payment_intent['client_secret']
   }.to_json
 end
--- src/Metazoo.js
+++ src/Metazoo.js
@@ -1,5 +1,40 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image'
+import { Card, Button } from "react-bootstrap";
+
+
+const products = {
+    products: [{
+        id: 1,
+        title: "Wilderness Booster Box",
+        price: 80,
+        img_url: "https://cdn.shopify.com/s/files/1/0615/2857/2148/products/wilderness.jpg?v=1649619658&width=600",
+        description: "36 Booster Packs!"
+    }, 
+    {
+        id: 2,
+        title: "Seance Booster Box",
+        price: 80,
+        img_url: "https://cdn.shopify.com/s/files/1/0615/2857/2148/products/seance_2d488d72-5fb3-42db-94f3-1bc94fb2597b.jpg?v=1666925181&width=600",
+        description: "36 Booster Packs!"
+    }]
+}
 
 export default function Metazoo(){
-    return <div><h1>Metazoo</h1></div>
+    return (<div>
+    {products.products.map((product) => (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={product.img_url} />
+      <Card.Body>
+        <Card.Title>{product.title}</Card.Title>
+        <Card.Text>
+            {product.price}
+        </Card.Text>
+        <Button variant="primary">Add to Cart</Button>
+      </Card.Body>
+    </Card>
+    )
+        )}
+        </div>)
 }
